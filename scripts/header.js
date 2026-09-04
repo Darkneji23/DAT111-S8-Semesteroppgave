5e79d150faaf15252027fcb77cb54d1d847d2d68
Simien <simien.mosland@gmail.com>
2026-09-04 10:29:50 +0200

    header blir lagt til av script

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,78 @@
-const hamburgerMenuContainer = document.getElementById("hamburger-menu-container");
-const hamburgerMenuBtn = document.getElementById("hamburger-menu-btn");
-const hamburgerMenuBg = document.getElementById("hamburger-menu-bg");
+//List of links shown directly on the header
+const headerLinks = [
+    { href: "", text: "Hjem" },
+    { href: "", text: "Om-oss" },
+    // { href: "", text: "Oppusing" },
+    // { href: "", text: "Utvendig" },
+];
 
+//List of links added to the hamburger menu
+const menuLinks = [
+    { href: "", text: "Hjem" },
+    { href: "", text: "Om-oss" },
+    { href: "", text: "Kontakt-oss" },
+    // { href: "", text: "Oppusing" },
+    // { href: "", text: "Utvendig" },
+];
+
+//Creates logo element
+const logo = document.createElement("img");
+logo.classList.add("logo");
+logo.src = "./images/logo.png";
+
+//Creates nav element with links directly on the header
+const nav = document.createElement("nav");
+headerLinks.forEach(link => {
+    const el = document.createElement("a");
+    el.append(link.text);
+    el.href = link.href;
+    nav.appendChild(el);
+});
+
+//Creates the hamburger menu button
+const hamburgerMenuBtn = document.createElement("button");
+hamburgerMenuBtn.classList.add("hamburger-icon");
+hamburgerMenuBtn.appendChild(document.createElement("div"));
+hamburgerMenuBtn.appendChild(document.createElement("div"));
+hamburgerMenuBtn.appendChild(document.createElement("div"));
+
+//Creates the hamburger menu that opens
+const hamburgerMenuBg = document.createElement("div");
+hamburgerMenuBg.classList.add("menu-background");
+const menu = document.createElement("div");
+menu.classList.add("menu");
+const menuList = document.createElement("ul");
+menu.appendChild(menuList);
+menuLinks.forEach(link => {
+    const a = document.createElement("a");
+    a.append(link.text);
+    a.href = link.href;
+    const li = document.createElement("li");
+    li.appendChild(a);
+    menuList.appendChild(li);
+});
+
+const hamburgerMenuContainer = document.createElement("div");
+hamburgerMenuContainer.classList.add("hamburger-menu");
+hamburgerMenuContainer.appendChild(hamburgerMenuBtn);
+hamburgerMenuContainer.appendChild(hamburgerMenuBg);
+hamburgerMenuContainer.appendChild(menu);
+
+//Creates and adds elements to the header itself
+const headerContent = document.createElement("div");
+headerContent.classList.add("header-content");
+headerContent.appendChild(logo);
+headerContent.appendChild(nav);
+headerContent.appendChild(hamburgerMenuContainer);
+
+const header = document.createElement("header");
+header.appendChild(headerContent)
+
+//Ads header to the body
+const body = document.querySelector("body");
+body.insertBefore(header, body.firstChild);
+
+//Adds eventlistneres listening for clicks
 hamburgerMenuBtn.addEventListener("click", (e) => {
     ToggleHamburgerMenu(e.target.parentElement)
 });
@@ -10,10 +81,10 @@ hamburgerMenuBg.addEventListener("click", (e) => {
     hamburgerMenuContainer.classList.remove("open")
 });
 
-function ToggleHamburgerMenu(){
-    if(hamburgerMenuContainer.classList.contains("open")){
+function ToggleHamburgerMenu() {
+    if (hamburgerMenuContainer.classList.contains("open")) {
         hamburgerMenuContainer.classList.remove("open")
-    }else{
+    } else {
         hamburgerMenuContainer.classList.add("open")
     }
 }
